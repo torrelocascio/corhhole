87d23e013342eab623723726d6077b87a70943d8
Louis Medina <louismedina@Louiss-MacBook-Pro.local>
2018-02-23 16:32:53 -0500

    Display All Campaigns

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const Campaign = require('../models/campaign');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  Campaign
+    // Retrieve all existing campaigns
+    .find({})
+    .populate('_creator')
+    .exec((err, campaigns) => {
+      res.render('index', { campaigns });
+    });
 });
 
 module.exports = router;
